758e5b0742f67215c10fd0bf71e8006a4ab82efe
KimJangHyeok <48301140+d0orHyeok@users.noreply.github.com>
2021-10-24 07:55:53 +0900

    Add event in Subject components

--- src/App.js
+++ src/App.js
@@ -8,7 +8,9 @@ class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            mode: "welcome",
             subject: { title: "WEB", sub: "World Wide Web!" },
+            welcome: { title: "Welcome", desc: "Hello, React!" },
             contents: [
                 { id: 1, title: "HTML", desc: "HTML is HyperText ..." },
                 { id: 2, title: "CSS", desc: "CSS is design ..." },
@@ -17,11 +19,26 @@ class App extends Component {
         };
     }
     render() {
+        let _title,
+            _desc = null;
+        if (this.state.mode === "welcome") {
+            _title = this.state.welcome.title;
+            _desc = this.state.welcome.desc;
+        } else if (this.state.mode === "read") {
+            _title = this.state.contents[0].title;
+            _desc = this.state.contents[0].desc;
+        }
         return (
             <div className="app">
-                <Subject title={this.state.subject.title} sub={this.state.subject.sub}></Subject>
+                <Subject
+                    title={this.state.subject.title}
+                    sub={this.state.subject.sub}
+                    onChangePage={() => {
+                        this.setState({ mode: "welcome" });
+                    }}
+                ></Subject>
                 <TOC data={this.state.contents}></TOC>
-                <Content title="HTML" desc="HTML is HyperText Markup Language."></Content>
+                <Content title={_title} desc={_desc}></Content>
             </div>
         );
     }
